3077af45c25f83935c7e517903eceb34489f62ed
jar9332 <47160979+jar9332@users.noreply.github.com>
2022-03-18 15:27:53 -0400

    Login and car registration validates

--- src/scenes/createAccount/index.js
+++ src/scenes/createAccount/index.js
@@ -4,14 +4,23 @@ import { useState } from "react";
 import TextField from "../../components/atoms/text-fields/text-field";
 import { CheckBox } from "../../components/atoms/checkboxes/index";
 import { BackArrow } from "../../components/atoms/arrows";
+import { validate, validatePass } from "../../utils/validate";
 import "./style.css";
 
 const CreateAccount = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [firstname, setfirstname] = useState("");
-  const [lastname, setlastname] = useState("");
-  const [phone, setphone] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordConfirmError, setPasswordConfirmError] = useState(false);
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
 
   const handleEmailInput = (e) => {
     const { value } = e.currentTarget;
@@ -25,42 +34,89 @@ const CreateAccount = () => {
       value,
     });
   };
+  const handlePasswordConfirmInput = (e) => {
+    const { value } = e.currentTarget;
+    setPasswordConfirm({
+      value,
+    });
+  };
   const handleFirstNameInput = (e) => {
     const { value } = e.currentTarget;
-    setfirstname({
+    setFirstName({
       value,
     });
   };
   const handleLastNameInput = (e) => {
     const { value } = e.currentTarget;
-    setlastname({
+    setLastName({
       value,
     });
   };
   const handlePhoneInput = (e) => {
     const { value } = e.currentTarget;
-    setphone({
+    setPhone({
       value,
     });
   };
 
   function create() {
-    fetch("https://revperformance-dev.ryacom.org/api/user/register", {
-      method: "POST",
-      headers: {
-        Accept: "*/*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        first_name: firstname.value,
-        last_name: lastname.value,
-        pswd: password.value,
-        phone: phone.value,
-        email: email.value,
-      }),
-    }).then((value) => {
-      console.log(value);
-    });
+    let flag = true;
+
+    if (validate(firstName)) {
+      setFirstNameError(false);
+    } else {
+      setFirstNameError(true);
+      flag = false;
+    }
+    if (validate(lastName)) {
+      setLastNameError(false);
+    } else {
+      setLastNameError(true);
+      flag = false;
+    }
+    if (validate(email)) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+      flag = false;
+    }
+    if (validate(phone)) {
+      setPhoneError(false);
+    } else {
+      setPhoneError(true);
+      flag = false;
+    }
+    if (validate(password)) {
+      setPasswordError(false);
+    } else {
+      setPasswordError(true);
+      flag = false;
+    }
+    if (validatePass(passwordConfirm)) {
+      setPasswordConfirmError(false);
+    } else {
+      setPasswordConfirmError(true);
+      flag = false;
+    }
+
+    if (flag) {
+      fetch("https://revperformance-dev.ryacom.org/api/user/register", {
+        method: "POST",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName.value,
+          last_name: lastName.value,
+          pswd: password.value,
+          phone: phone.value,
+          email: email.value,
+        }),
+      }).then((value) => {
+        console.log(value);
+      });
+    }
   }
 
   return (
@@ -70,22 +126,30 @@ const CreateAccount = () => {
       <div>
         <form>
           <label className="label__create__account">First Name</label>
-          <TextField onChange={handleFirstNameInput}></TextField>
+          <TextField
+            onChange={handleFirstNameInput}
+            error={firstNameError}
+          ></TextField>
           <label className="label__create__account">Last Name</label>
-          <TextField onChange={handleLastNameInput}></TextField>
+          <TextField
+            onChange={handleLastNameInput}
+            error={lastNameError}
+          ></TextField>
           <label className="label__create__account">Email</label>
-          <TextField onChange={handleEmailInput}></TextField>
+          <TextField onChange={handleEmailInput} error={emailError}></TextField>
           <label className="label__create__account">Phone</label>
-          <TextField onChange={handlePhoneInput}></TextField>
+          <TextField onChange={handlePhoneInput} error={phoneError}></TextField>
           <label className="label__create__account">Password</label>
           <TextField
             onChange={handlePasswordInput}
             type={"password"}
+            error={passwordError}
           ></TextField>
           <label className="label__create__account">Confirm Password</label>
           <TextField
-            onChange={handlePasswordInput}
+            onChange={handlePasswordConfirmInput}
             type={"password"}
+            error={passwordConfirmError}
           ></TextField>
           <SecondaryButton
             text={"CREATE ACCOUNT "}
---
+++ src/utils/validate.js
@@ -0,0 +1,13 @@
+export const validate = (text) => {
+  text != ""
+    ? console.log(text + " validated")
+    : console.log(text + " invalidated");
+  return text != "";
+};
+
+export const validatePass = (pass1, pass2) => {
+  pass1 == pass2
+    ? console.log(pass2 + " validated")
+    : console.log(pass2 + " invalidated");
+  return pass1 == pass2;
+};
